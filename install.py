0f19319949de6523cbdae423d0c0aa5c40454ea6
hollis7 <herryhollis@163.com>
2023-10-19 17:26:14 +0800

    install env test

---
+++ install.py
@@ -0,0 +1,24 @@
+# %%
+import sys
+
+sys.version
+# %%
+import torch
+import torchvision
+import torchaudio
+
+print(torch.__version__), torchvision.__version__, torchaudio.__version__
+# %%
+import datasets
+
+print(datasets.__version__)
+# %%
+import transformers
+
+print(transformers.__version__)
+# %%
+import datasets
+
+print(datasets.__version__)
+
+# %%
