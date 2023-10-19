1f61b999681b6f5acf42f5b204051ebc026b11f2
hollis7 <herryhollis@163.com>
2023-10-19 17:25:54 +0800

    use cuda for cn_classify

--- cn_classify_cuda.py
+++ cn_classify_cuda.py
@@ -61,3 +61,141 @@ model(input_ids=input_ids,
 #后面的计算和中文分类完全一样，只是放在了cuda上计算
 
 # %%
+from datasets import load_dataset, load_from_disk
+
+
+#定义数据集
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, split):
+        self.dataset = load_dataset(path='lansinuote/ChnSentiCorp', split=split)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, i):
+        text = self.dataset[i]['text']
+        label = self.dataset[i]['label']
+
+        return text, label
+
+
+dataset = Dataset('train')
+
+len(dataset), dataset[0]
+# %%
+from transformers import BertTokenizer
+
+#加载字典和分词工具
+token = BertTokenizer.from_pretrained('bert-base-chinese')
+
+
+def collate_fn(data):
+    sents = [i[0] for i in data]
+    labels = [i[1] for i in data]
+
+    #编码
+    data = token.batch_encode_plus(batch_text_or_text_pairs=sents,
+                                   truncation=True,
+                                   padding='max_length',
+                                   max_length=500,
+                                   return_tensors='pt',
+                                   return_length=True)
+
+    #input_ids:编码之后的数字
+    #attention_mask:是补零的位置是0,其他位置是1
+    input_ids = data['input_ids'].to(device)
+    attention_mask = data['attention_mask'].to(device)
+    token_type_ids = data['token_type_ids'].to(device)
+    labels = torch.LongTensor(labels).to(device)
+
+    #print(data['length'], data['length'].max())
+
+    return input_ids, attention_mask, token_type_ids, labels
+
+
+#数据加载器
+loader = torch.utils.data.DataLoader(dataset=dataset,
+                                     batch_size=16,
+                                     collate_fn=collate_fn,
+                                     shuffle=True,
+                                     drop_last=True)
+
+for i, (input_ids, attention_mask, token_type_ids,
+        labels) in enumerate(loader):
+    break
+
+print(len(loader))
+input_ids.shape, attention_mask.shape, token_type_ids.shape, labels
+# %%
+from transformers import AdamW
+
+#训练
+optimizer = AdamW(model.parameters(), lr=5e-4)
+criterion = torch.nn.CrossEntropyLoss()
+
+model.train()
+for i, (input_ids, attention_mask, token_type_ids,
+        labels) in enumerate(loader):
+    out = model(input_ids=input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids)
+
+    loss = criterion(out, labels)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    if i % 5 == 0:
+        out = out.argmax(dim=1)
+        accuracy = (out == labels).sum().item() / len(labels)
+
+        print(i, loss.item(), accuracy)
+
+    if i == 100:
+        break
+
+
+# %%
+model_state_dict = model.state_dict()
+torch.save(model_state_dict, 'cn_classify_cuda.pt')
+
+# %%
+model_state_dict = model.load('cn_classify_cuda.pt')
+model.load_state_dict(model_state_dict)
+# %%
+#测试
+def test():
+    model.eval()
+    correct = 0
+    total = 0
+
+    loader_test = torch.utils.data.DataLoader(dataset=Dataset('validation'),
+                                              batch_size=32,
+                                              collate_fn=collate_fn,
+                                              shuffle=True,
+                                              drop_last=True)
+
+    for i, (input_ids, attention_mask, token_type_ids,
+            labels) in enumerate(loader_test):
+
+        if i == 5:
+            break
+
+        print(i)
+
+        with torch.no_grad():
+            out = model(input_ids=input_ids,
+                        attention_mask=attention_mask,
+                        token_type_ids=token_type_ids)
+
+        out = out.argmax(dim=1)
+        correct += (out == labels).sum().item()
+        total += len(labels)
+
+    print(correct / total)
+
+
+test()
+
+# %%
